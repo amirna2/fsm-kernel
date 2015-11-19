61751fef9ac23ca54ba409393dcc209e0f35b662
Amir Nathoo <amir.nathoo@yahoo.com>
2015-11-19 13:17:07 -0800

    Update fsm_private.h

--- fsm_private.h
+++ fsm_private.h
@@ -1,3 +1,4 @@
+/*
 Copyright (c) 2000-2014, Amir Nathoo <amir.nth@gmail.com>
 All rights reserved.
 
@@ -22,12 +23,12 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 /*
 * fsm_private.h
 *
-*  Created on:  Jan,25th 2014 
-*      Author:  Amir Nathoo
+*  File created on:  Jan 25th, 2014 
 *
 *  Description: Private header file for the Fsm Engine
 */
